40f9ead6fd20de406cdd84c1aed8c86e4c3644fc
frednirk <frednirk@gmx.com>
2024-04-30 21:15:44 +1000

    first commit

---
+++ fredbot.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import os
+from groq import Groq
+import random
+
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os 
+
+load_dotenv('.env')
+
+# Load GROQ API Key
+groq_api_key = os.environ['GROQ_API_KEY']
+
+# the main program 
+def main():
+
+    st.title("Fred Chat Bot")
+
+    memory=ConversationBufferWindowMemory(value = 5)
+
+    user_question = st.text_area("Ask Fred a question:")
+
+    # session state variable
+    if 'chat_history' not in st.session_state:
+        st.session_state.chat_history=[]
+    else:
+        for message in st.session_state.chat_history:
+            memory.save_context({'input':message['human']},{'output':message['AI']})
+
+
+    # Initialize Groq Langchain chat object and conversation
+    groq_chat = ChatGroq(
+            groq_api_key=groq_api_key, 
+            model_name= "llama3-8b-8192"
+    )
+
+    conversation = ConversationChain(
+            llm=groq_chat,
+            memory=memory
+    )
+
+    if user_question:
+        response = conversation(user_question)
+        message = {'human':user_question,'AI':response['response']}
+        st.session_state.chat_history.append(message)
+        st.write("Fred :  ", response['response'])
+
+if __name__ == "__main__":
+    main()
